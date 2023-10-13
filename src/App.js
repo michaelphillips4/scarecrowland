deda7fba381d2c806350dbc2cf788074b3dc99bf
michaelp <michaelp@linux-laptop.lan>
2023-10-13 19:57:03 +0100

    Various

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 import Home from './parts/Home';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./parts/Header"
+/* import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 import Gallery from './parts/Gallery';
-import Layout from './layout';
+import Layout from './layout';*/
 
 function App() {
   return (
-  
-    <BrowserRouter>
-    <Routes>
+  <>
+  <Header />
+  <Home />
+  <br />
+<span>&copy; scarecrowland 2023</span>
+<br /> <br /> <br />
+  </>
+/*     <BrowserRouter>
+    <Routes basename={process.env.PUBLIC_URL}>
       <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
         <Route path="Gallery" element={<Gallery />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </BrowserRouter> */
 
   );
 }
--- src/parts/GallerysList.js
+++ src/parts/GallerysList.js
@@ -1,5 +1,5 @@
 import './GallerysList.css';
-import {Link } from "react-router-dom";
+/* import {Link } from "react-router-dom"; */
 
 function GalleryList() {
   return (
@@ -9,68 +9,69 @@ function GalleryList() {
       <p>
         <div className="gallerylist-flex-container">
           <div>
-            <Link to="Gallery">  Green Man Regents Park London</Link><br />
-            <a href="gal46.html">  March 2013</a><br />
-            <a href="gal45.html">  Harvest fertival 2012 Green Man</a><br />
-            <a href="gal43.html">  Olympic Team UK 2012</a><br />
-            <a href="gal44.html">  Various Scarecrows 2011-2012</a><br />
+            {/*  <Link to="Gallery">  Green Man Regents Park London</Link><br /> */}
+            <a href="http://www.scarecrowland.co.uk/gal48.html">  Green Man Regents Park London</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal46.html">  March 2013</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal45.html">  Harvest fertival 2012 Green Man</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal43.html">  Olympic Team UK 2012</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal44.html">  Various Scarecrows 2011-2012</a><br />
             <a href="http://regentsparkallotmentgarden.blogspot.co.uk/2011/10/harvest-festival-2011-part-2-scarecrows.html">Harvest fertival 2011</a><br />
             <a href="http://www.scarecrowland.co.uk/Pictures/gal42/index.html">  New Work 2011</a><br />
-            <a href="gal41.html">  Autum Brighton 2010</a><br />
-            <a href="gal40.html">  Summer 2010</a><br />
-            <a href="gal39.html">  FlowerPot Scarecrows</a><br />
-            <a href="gal38.html">  Harry found a new home in Kent</a><br />
-            <a href="gal37.html">  Various Scarecrows 2009</a><br />
-            <a href="gal36.html">  Celebrity Scarecrows 2008</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal41.html">  Autum Brighton 2010</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal40.html">  Summer 2010</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal39.html">  FlowerPot Scarecrows</a><br />
+            <a href="ghttp://www.scarecrowland.co.uk/al38.html">  Harry found a new home in Kent</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal37.html">  Various Scarecrows 2009</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal36.html">  Celebrity Scarecrows 2008</a><br />
           </div>
           <div>
-            <a href="gal35.html">  Scarecrows in the USA 2008</a><br />
-            <a href="gal34.html">  Fred in the USA 2008</a><br />
-            <a href="gal33.html">  Workshops around Sussex 2007</a><br />
-            <a href="gal32.html">  Scarecrows in actions 2007</a><br />
-            <a href="gal31.html">  Goldy's 2007</a><br />
-            <a href="gal30.html">  various sacrecrows in summer 2007</a><br />
-            <a href="gal29.html">  Creating atma 2007</a><br />
-            <a href="gal28.html">  summer solstice ventura California 2007</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal35.html">  Scarecrows in the USA 2008</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal34.html">  Fred in the USA 2008</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal33.html">  Workshops around Sussex 2007</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal32.html">  Scarecrows in actions 2007</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal31.html">  Goldy's 2007</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal30.html">  various sacrecrows in summer 2007</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal29.html">  Creating atma 2007</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal28.html">  summer solstice ventura California 2007</a><br />
 
-            <a href="gal27.html">  fiesta (montecito) ymca summer 2006</a><br />
-            <a href="gal26.html">  Pasadena Workshop summer 2006</a><br />
-            <a href="gal23.html">  Memorial may weekend</a><br />
-            <a href="gal22.html">  Montecito pre School Workshop</a><br />
-            <a href="gal21.html">  Montecito Family YMCA summer 2005</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal27.html">  fiesta (montecito) ymca summer 2006</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal26.html">  Pasadena Workshop summer 2006</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal23.html">  Memorial may weekend</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal22.html">  Montecito pre School Workshop</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal21.html">  Montecito Family YMCA summer 2005</a><br />
           </div>
-          <div><a href="gal20.html">  Hollister School Garden (Goleta)</a><br />
-            <a href="gal19.html">  more workshop at Hollister School</a><br />
-            <a href="gal18.html">  workshop at Hollister School</a><br />
-            <a href="gal17.html">  the four season <b>scarecrows</b></a><br />
+          <div><a href="http://www.scarecrowland.co.uk/gal20.html">  Hollister School Garden (Goleta)</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal19.html">  more workshop at Hollister School</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal18.html">  workshop at Hollister School</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal17.html">  the four season <b>scarecrows</b></a><br />
 
-            <a href="gal16.html">  various <b>scarecrows</b></a><br />
-            <a href="gal15.html">  fall and christmas in the usa </a><br />
-            <a href="gal14.html">  various in the usa</a><br />
-            <a href="gal12.html">  christmas in the usa.</a><br />
-            <a href="gal11.html">  work in the usa.</a><br />
-            <a href="gal10.html">  workshops in the uk.</a><br />
-            <a href="gal9.html">   a <b>scarecrow</b> for joe</a><br />
-            <a href="gal8.html">   workshops in usa.</a><br />
-            <a href="gal7.html">   advertising <b>scarecrows</b></a><br />
+            <a href="http://www.scarecrowland.co.uk/gal16.html">  various <b>scarecrows</b></a><br />
+            <a href="http://www.scarecrowland.co.uk/gal15.html">  fall and christmas in the usa </a><br />
+            <a href="http://www.scarecrowland.co.uk/gal14.html">  various in the usa</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal12.html">  christmas in the usa.</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal11.html">  work in the usa.</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal10.html">  workshops in the uk.</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal9.html">   a <b>scarecrow</b> for joe</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal8.html">   workshops in usa.</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal7.html">   advertising <b>scarecrows</b></a><br />
           </div>
           <div>
-            <a href="gal6.html">   celebrities and famous <b>scarecrows</b></a><br />
+            <a href="http://www.scarecrowland.co.uk/gal6.html">   celebrities and famous <b>scarecrows</b></a><br />
 
-            <a href="gal5.html">   traditional <b>scarecrows</b></a><br />
-            <a href="gal4.html">   various <b>scarecrows</b></a><br />
-            <a href="gal3.html">   various workshops &amp; events</a><br />
-            <a href="gal2.html">   lookalike and famous <b>scarecrows</b></a><br />
-            <a href="gal1.html">   an array of traditional <b>scarecrows</b></a><br />
+            <a href="http://www.scarecrowland.co.uk/gal5.html">   traditional <b>scarecrows</b></a><br />
+            <a href="http://www.scarecrowland.co.uk/gal4.html">   various <b>scarecrows</b></a><br />
+            <a href="http://www.scarecrowland.co.uk/gal3.html">   various workshops &amp; events</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal2.html">   lookalike and famous <b>scarecrows</b></a><br />
+            <a href="http://www.scarecrowland.co.uk/gal1.html">   an array of traditional <b>scarecrows</b></a><br />
 
 
             <b>Ceramics and Hats</b><br />
-            <a href="Cgal2.html">Ceramics 2003 -2004 </a><br />
-            <a href="Cgal1.html">Ceramics 2005</a><br />
-            <a href="gal24.html">Ceramics Summer 2005</a><br />
-            <a href="gal25.html">Ceramics Summer 2006</a><br />
+            <a href="http://www.scarecrowland.co.uk/Cgal2.html">Ceramics 2003 -2004 </a><br />
+            <a href="http://www.scarecrowland.co.uk/Cgal1.html">Ceramics 2005</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal24.html">Ceramics Summer 2005</a><br />
+            <a href="http://www.scarecrowland.co.uk/gal25.html">Ceramics Summer 2006</a><br />
             <a href="http://www.scarecrowland.co.uk/Pictures/Galc5/index.html">Ceramics 2011 </a><br />
-            <a href="hatgal1.html">  Hats New Range in Spring 2011</a>
+            <a href="http://www.scarecrowland.co.uk/hatgal1.html">  Hats New Range in Spring 2011</a>
           </div>
         </div>
       </p>
